ea4d49c103d849c6353cf72ae4cd0fed089e6157
anasmahmoud12 <es.anas.mahmoud2023@alexu.edu.eg>
2025-10-04 13:09:23 +0300

    Create uduino.ino

---
+++ uduino.ino
@@ -0,0 +1,90 @@
+String inputstr="";
+    bool strcom=false;
+    int x=1;
+    int bin3=3;int unit=1000;
+    String morse[]={
+  ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---",
+  "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-",
+  "..-", "...-", ".--", "-..-", "-.--", "--.."};
+  String code;
+
+void setup(){
+  Serial.begin(9600);
+  Serial.println("type the textt  ");
+  pinMode(bin3,OUTPUT);
+}
+
+
+void loop (){
+
+ 
+  if (strcom&&x==1){
+    --x;
+    inputstr.toUpperCase(); 
+ Serial.println(inputstr);   
+    for (int i=0;i<inputstr.length();++i){
+        char a=inputstr.charAt(i);
+        if (a==' '){
+space();
+continue;
+        }
+
+
+code=morse[a-'A'];
+for (int j=0;j<code.length();++j){
+    char c=code.charAt(j);
+    if (c=='.'){
+        dot(code.length(),j);
+
+    }
+    else if (c=='-'){
+        dash(code.length(),j);
+    }
+}
+if (i!=inputstr.length()-1){
+if(inputstr[i+1]!=' '){
+    letter();
+}
+}
+
+    }
+  
+
+}
+
+
+  
+}
+
+
+void dot(int n,int j){
+  digitalWrite(bin3,HIGH);
+  delay(unit);
+    digitalWrite(bin3,LOW);
+  if (j!=n-1){
+delay(unit);
+}}
+
+
+void dash(int n ,int j){
+  digitalWrite(bin3,HIGH);
+  delay(3*unit);
+    digitalWrite(bin3,LOW);
+ if (j!=n-1){
+delay(unit);
+}
+}
+void space(){
+  delay(7*unit);
+}
+void letter(){
+  delay(3*unit);
+}
+
+void serialEvent() {
+  while (Serial.available()) {
+    char inChar = (char)Serial.read();
+    if (inChar == '\n') strcom = true;
+    else inputstr += inChar;
+  }
+}
