1ec7e9346885e171ad3acd97938e8aa34e198690
anasmahmoud12 <es.anas.mahmoud2023@alexu.edu.eg>
2025-10-18 19:15:31 +0300

    Update sin.ino

--- sin.ino
+++ sin.ino
@@ -1,15 +1,19 @@
 float t = 0;
 unsigned long last = 0;
 
+float dt = 0.02;        
+unsigned long interval;   
+
 void setup() {
   Serial.begin(115200);
+  interval = dt * 1000; 
 }
 
 void loop() {
-  if (millis() - last > 25) {
+  if (millis() - last >= interval) {
     last = millis();
-    float y = sin(6.28  * t);
+    float y = sin(6.283 * t);  
     Serial.println(y);
-    t += 0.02;
+    t += dt;
   }
 }
